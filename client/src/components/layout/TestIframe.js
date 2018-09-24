e5525e5adc39a7a21e92c2b5a4415228129869bb
Zihan Zhang <zzhlovesly@outlook.com>
2018-09-24 20:49:38 +0800

    hm

--- client/src/components/layout/TestIframe.js
+++ client/src/components/layout/TestIframe.js
@@ -5,7 +5,8 @@ export default () => {
     return (
             <div id="my-canvas" style={{ width: '100%', height:'600px' }}>
                 <iframe 
-                    src="/perlintest/indexTest.html/"
+                    // src="/perlintest/indexTest.html/"
+                    src="https://my-site-zz.herokuapp.com/perlintest/indexTest.html"
                     // src="https://www.youtube.com/embed/xDMP3i36naA/"
                     width="100%" 
                     height="100%" 
